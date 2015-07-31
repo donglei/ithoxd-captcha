671bf84424599ade849a8a83e535cc00640f929c
root <root@debiandong>
2015-07-31 15:42:59 +0800

    render captcha

--- source/app.d
+++ source/app.d
@@ -3,9 +3,15 @@ import ithox.captcha;
 
 void main()
 {
-	auto captcha = new IthoxCaptcha(100, 50);
-	captcha.length = 5;
-	captcha.render();
+	auto captcha = new IthoxCaptcha(125,42);
+	captcha.length = 6;
+	captcha.fontSize = 30;
+	captcha.drawCode();
+	captcha.save("./cache/" ~ captcha.code ~ ".gif");
 	import std.experimental.logger.core;
+	//log(cast(string)captcha.render("gif"));
+	log(captcha.renderBase64("gif"));
 	logf("width is %s code is %s", captcha.width, captcha.code);
+
+
 }
--- source/ithox/captcha/captcha.d
+++ source/ithox/captcha/captcha.d
@@ -10,6 +10,11 @@ import std.experimental.logger.core;
 
 ///验证码使用到的字符串
 package enum ITHOX_CAPTCHA_CHARACTER = "abcdefghkmnprstuvwyzABCDEFGHKLMNPRSTUVWYZ23456789";
+///颜色库
+package enum string[] ITHOX_CAPTCHA_COLOR = ["red", "blue", "gree", "#FF4040", "#F08080", "#4B0082", "#9B30FF", "#8B5A00", "#7FFF00", "#48D1CC", "#00C5CD", "#AB82FF", "#8B2323"];
+///字体
+package enum string[] ITHOX_CAPTCHA_FONT = ["Candice", "Ravie"];
+
 
 class IthoxCaptcha{
 	private {
@@ -17,14 +22,18 @@ class IthoxCaptcha{
 		int _width = 100;
 		///高
 		int _height = 36;
+
 		///背景色
 		string _background = "#D1EEEE";
 		///验证码长度
 		ubyte _length = 4;
-		///字体
-		ubyte _fontSize = 18;
+		///字体大小
+		int _fontSize = 18;
 		///验证码
 		string _code ="donglei";
+		///
+		size_t _fontWeight = 10;
+
 		///图片
 		Image image;
 		///
@@ -60,6 +69,22 @@ class IthoxCaptcha{
 	{
 		return this._height;
 	}
+	@property void fontSize(int _h)
+	{
+		this._fontSize = _h;
+	}
+	@property int fontSize()
+	{
+		return this._fontSize;
+	}
+	@property void fontWeight(size_t _h)
+	{
+		this._fontWeight = _h;
+	}
+	@property size_t fontWeight()
+	{
+		return this._fontWeight;
+	}
 
 	@property void background(string _background)
 	{
@@ -86,32 +111,60 @@ class IthoxCaptcha{
 		return this._length;
 	}
 
-	@property void fontSize(ubyte _size)
+
+	///输出图片 base64 压缩的图片image_format =  jpg png
+	void[] render(string image_format = "gif")
 	{
-		this._fontSize = _size;
-	}
-	@property ubyte fontSize()
-	{
-		return this._fontSize;
+		assert(image !is null, "call drawCode function before save");
+		return image.toBlob(image_format);
 	}
 
-	///输出图片 base64 压缩的图片
-	string render()
+	///输出图片 base64 压缩的图片image_format =  jpg png
+	string renderBase64(string image_format = "gif")
+	{
+		import std.base64;
+		auto encode64 = Base64.encode(cast(ubyte[])render(image_format));
+		import std.format;
+		return format("data:%s;base64,%s", getImageContentType(image_format),cast(string)encode64);
+	}
+
+	//输出到图片
+	public void save(string fileName)
+	{
+		assert(image !is null, "call drawCode function before save");
+		image.write(fileName);
+	}
+	//生图
+	public void drawCode()
 	{
 		this._code = "";
 		randCode();
 		this.draw = new DrawingContext();
 		image = new Image(Geometry(_width, _height),new Color(_background));
-		image.addNoise(NoiseType.ImpulseNoise);
-		
-			draw.text(10, 20, _code);
-			draw.fontSize(20);
-			draw.fontWeight(10);
-			draw.draw(image);
-		image.write(_code ~ ".png");
+		//image.addNoise(NoiseType.MultiplicativeGaussianNoise);
 
-		logf("code name  is %s ", _code ~ ".png");
-		return "";
+		//draw line
+		for(int i =0; i < uniform(5, 20); i ++)
+		{
+			draw.line(uniform(1, _width), uniform(1, _height), uniform(1, _width), uniform(1, _height));
+			draw.fill(new Color(ITHOX_CAPTCHA_COLOR[uniform(0, ITHOX_CAPTCHA_COLOR.length)]));
+		}
+		//draw code
+		int per_width = _width / _length;
+		int half_height = _height / 2 + _fontSize / 2;
+
+		for(int i = 0; i < _length; i ++)
+		{
+			draw.fontSize(_fontSize);
+			draw.fontWeight(_fontWeight);
+			draw.fontStyle(uniform(0, 2) == 0 ? StyleType.ItalicStyle : StyleType.NormalStyle);
+			draw.font(ITHOX_CAPTCHA_FONT[uniform(0, ITHOX_CAPTCHA_FONT.length)]);
+			draw.fill(new Color(ITHOX_CAPTCHA_COLOR[uniform(0, ITHOX_CAPTCHA_COLOR.length)]));
+
+			draw.text(per_width * i + uniform(0, (per_width) / 3), half_height + uniform(-1, 1) * uniform(0, 10), this._code[i] ~ "");
+		}
+
+		draw.draw(image); 
 	}
 	///生成随机吗
 	private void randCode()
@@ -122,8 +175,26 @@ class IthoxCaptcha{
 		}
 	}
 	
+	///获取图片类型
+	string getImageContentType(string ext)
+	{
+		 switch(ext)
+		{
+			case "gif":
+				return "image/gif";
+			case "png":
+				return "image/png";
+			case "jpg":
+				return "image/jpg";
+			case "webp":
+				return "image/webp";
+			default:
+				assert(false, "Unsupport image type");
+		}
+	}
 
 }
+
 
 unittest{
 	auto captcha = new IthoxCaptcha(100, 50);
